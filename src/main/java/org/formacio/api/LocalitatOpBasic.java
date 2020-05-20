4d5aed26bfdd6a8aae802436cbe0887752e11054
carloshoyos98 <choyos@cifpfbmoll.eu>
2020-05-20 18:35:58 +0200

    feat(LocalitatOpBasic): Implementar métodos alta y elimina

--- src/main/java/org/formacio/api/LocalitatOpBasic.java
+++ src/main/java/org/formacio/api/LocalitatOpBasic.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
@@ -27,11 +28,18 @@ public class LocalitatOpBasic {
 		//Con localitat.orElse() indicamos que devuelva ese valor por defecto si Localitat no existe.
 		return localitat.orElse(new Localitat(0L, "", 0));
 	}
-	
+	@Transactional
 	public void alta (String nom, Integer habitants) {
+		Localitat novaLocalitat = new Localitat();
+		novaLocalitat.setNom(nom);
+		novaLocalitat.setHabitants(habitants);
+		em.persist(novaLocalitat);
 	}
 	
+	@Transactional
 	public void elimina (long id) {
+		Localitat localitat = em.find(Localitat.class, id);
+		em.remove(localitat);
 	}
 	
 	public void modifica (Localitat localitat) {
