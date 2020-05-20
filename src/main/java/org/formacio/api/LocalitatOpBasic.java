325df2fa27a5c686876882db89adf399d1d9f655
carloshoyos98 <choyos@cifpfbmoll.eu>
2020-05-20 16:32:17 +0200

    Commit inicial y primer caso test pasado

---
+++ src/main/java/org/formacio/api/LocalitatOpBasic.java
@@ -0,0 +1,23 @@
+package org.formacio.api;
+
+/**
+ * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
+ * operacions de persistencia tal com indiquen les firmes dels metodes
+ */
+
+public class LocalitatOpBasic {
+	
+	public Localitat carrega (long id) {
+		return null;
+	}
+	
+	public void alta (String nom, Integer habitants) {
+	}
+	
+	public void elimina (long id) {
+	}
+	
+	public void modifica (Localitat localitat) {
+	}
+
+}
